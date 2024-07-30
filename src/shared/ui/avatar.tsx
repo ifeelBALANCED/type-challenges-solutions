b33375d33a4e4f9800af39539de60d93cda2b327
balanced <vladyslav.herasymenko@infotech.gov.ua>
2024-07-30 15:00:54 +0300

    chore(app): init challenges template

---
+++ src/shared/ui/avatar.tsx
@@ -0,0 +1,49 @@
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react'
+
+import { cn } from '@/shared/lib/cn'
+
+const AvatarBase = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
+    {...props}
+  />
+))
+AvatarBase.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Image>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image ref={ref} className={cn('aspect-square h-full w-full', className)} {...props} />
+))
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
+
+const AvatarFallback = forwardRef<
+  ElementRef<typeof AvatarPrimitive.Fallback>,
+  ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn('flex h-full w-full items-center justify-center rounded-full bg-muted', className)}
+    {...props}
+  />
+))
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+const Avatar = ({ src, alt, fallbackText }: { src: string; alt: string; fallbackText: string }) => {
+  return (
+    <AvatarBase>
+      <AvatarImage src={src} alt={alt} />
+      <AvatarFallback>{fallbackText}</AvatarFallback>
+    </AvatarBase>
+  )
+}
+
+Avatar.displayName = 'Avatar'
+
+export { Avatar, AvatarBase, AvatarFallback, AvatarImage }
